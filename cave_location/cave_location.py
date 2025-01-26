ec254ee19f02535a8f414d45fe5c1a7df32a839c
Yarusova Maria <yarusova.maria@yandex.ru>
2025-01-26 12:54:54 +0300

    пещера - локация(tiled)

---
+++ cave_location/cave_location.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+import pytmx
+
+# Инициализация Pygame
+pygame.init()
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Тайловая карта с pytmx")
+
+# Загрузка тайловой карты
+tmx_data = pytmx.load_pygame('cave.tmx')
+offset_x = 0
+scroll_speed = 1
+map_width = tmx_data.width * tmx_data.tilewidth
+max_offset_x = map_width - width
+
+# Основной цикл
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        offset_x += scroll_speed
+
+    if keys[pygame.K_LEFT]:
+        offset_x -= scroll_speed
+
+        if offset_x > max_offset_x:
+            offset_x = max_offset_x
+
+    screen.fill((255, 255, 255))
+    for layer in tmx_data.visible_layers:
+        for x, y, gid in layer:
+            if gid:
+                tile = tmx_data.get_tile_image_by_gid(gid)
+                screen.blit(tile, (x * tmx_data.tilewidth - offset_x, y * tmx_data.tileheight))
+    pygame.display.flip()
+pygame.quit()
+sys.exit()
