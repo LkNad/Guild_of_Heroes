4086fe39785b341228d3f055d4944bf7566a5d77
Yarusova Maria <yarusova.maria@yandex.ru>
2025-01-27 17:56:24 +0300

    локация леса(tiled)

---
+++ forest_location/data/rock(3).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="rock(3)" tilewidth="50" tileheight="50" tilecount="56" columns="8">
+ <image source="rock(3).png" width="430" height="371"/>
+</tileset>
---
+++ forest_location/data/rock_tree(6).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="rock_tree(6)" tilewidth="50" tileheight="50" tilecount="50" columns="10">
+ <image source="rock_tree(6).png" width="501" height="299"/>
+</tileset>
