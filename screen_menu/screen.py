066565646f5f0455bc9cfc3e637b17e8aef840dd
Yarusova Maria <yarusova.maria@yandex.ru>
2025-01-31 13:27:11 +0300

    слияние входа и меню

---
+++ screen_menu/screen.py
@@ -0,0 +1,109 @@
+import sys
+import sqlite3
+import threading
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QMessageBox, QLabel)
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from menu import MenuWindow  # Импортируем MenuWindow
+
+# Создание базы данных
+conn = sqlite3.connect('data/users.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS users (
+                username TEXT PRIMARY KEY,
+                password TEXT)''')
+conn.commit()
+
+class Screen(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Guild of Heroes")
+        self.setGeometry(1200, 900, 500, 400)
+
+        # Установка фона
+        self.background_label = QLabel(self)
+        self.background_pixmap = QPixmap("data/fon_1.jpg")
+        self.background_label.setPixmap(self.background_pixmap)
+        self.background_label.setScaledContents(True)
+        self.background_label.setGeometry(0, 0, 500, 400)
+
+        # Надпись
+        self.title_label = QLabel("Guild of Heroes", self)
+        self.title_label.setAlignment(Qt.AlignCenter)
+        self.title_label.setGeometry(0, 0, 500, 400)
+
+        # Обработчик нажатия
+        self.mousePressEvent = self.on_mouse_press
+
+    def on_mouse_press(self, event):
+        self.close()
+        self.main_window = MainWindow()
+        self.main_window.show()
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Вход")
+        self.setGeometry(1200, 900, 500, 400)
+
+        self.layout = QVBoxLayout()
+
+        self.username_input = QLineEdit(self)
+        self.username_input.setPlaceholderText("Имя пользователя")
+        self.layout.addWidget(self.username_input)
+
+        self.password_input = QLineEdit(self)
+        self.password_input.setPlaceholderText("Пароль")
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.layout.addWidget(self.password_input)
+
+        self.login_button = QPushButton("Войти", self)
+        self.login_button.clicked.connect(self.login)
+        self.layout.addWidget(self.login_button)
+
+        self.register_button = QPushButton("Зарегистрироваться", self)
+        self.register_button.clicked.connect(self.register)
+        self.layout.addWidget(self.register_button)
+
+        self.setLayout(self.layout)
+
+    def login(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = c.fetchone()
+        if user:
+            self.open_menu()
+        else:
+            QMessageBox.warning(self, "Ошибка", "Неверное имя пользователя или пароль!")
+
+    def register(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+        try:
+            c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            conn.commit()
+            QMessageBox.information(self, "Успех", "Регистрация успешна!")
+            self.open_menu()
+        except sqlite3.IntegrityError:
+            QMessageBox.warning(self, "Ошибка", "Имя пользователя уже существует!")
+
+    def open_menu(self):
+        threading.Thread(target=self.run_menu).start()  # Запуск меню в отдельном потоке
+        self.close()
+
+    def run_menu(self):
+        menu_window = MenuWindow()  # Создание экземпляра MenuWindow
+        menu_window.run()  # Запуск меню
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    screen = Screen()
+    screen.show()
+    sys.exit(app.exec_())
+
+# Закрытие соединения с базой данных
+conn.close()
+
+
