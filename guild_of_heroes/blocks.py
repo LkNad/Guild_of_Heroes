c08f613e091df2b5f0356ce3cd1d17b5020bc507
Nila <nila.ivonina@gmail.com>
2025-02-06 20:15:47 +0300

    добавила блоков

--- guild_of_heroes/blocks.py
+++ guild_of_heroes/blocks.py
@@ -6,7 +6,7 @@ PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = "#FF6262"
 
 
-class Platform(sprite.Sprite):
+class Platform_1(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
@@ -14,9 +14,17 @@ class Platform(sprite.Sprite):
         self.image = pygame.image.load("block_1.png")
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
 
-
-class Next_level(Platform):
+class Platform_2(sprite.Sprite):
     def __init__(self, x, y):
-        Platform.__init__(self, x, y)
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.image = pygame.image.load("block_2.jpg")
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+
+
+class Next_level(Platform_1):
+    def __init__(self, x, y):
+        Platform_1.__init__(self, x, y)
         self.image = pygame.image.load("next_level.png")
 
