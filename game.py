bc4f73d31e50ba5a2e9867f652ddbb8c22f1f9f4
Nila <nila.ivonina@gmail.com>
2025-02-06 19:27:28 +0300

    основной цикл игры

---
+++ game.py
@@ -0,0 +1,165 @@
+# Импортируем библиотеку pygame
+import sys
+
+import pygame
+from pygame import *
+
+from guild_of_heroes.screen_texts import win
+from player import *
+from blocks import *
+
+# Объявляем переменные
+WIN_WIDTH = 800  # Ширина создаваемого окна
+WIN_HEIGHT = 600  # Высота
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
+BACKGROUND_IMAGE = pygame.image.load('forest_bg.png')
+BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (WIN_WIDTH, WIN_HEIGHT))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l + WIN_WIDTH / 2, -t + WIN_HEIGHT / 2
+
+    l = min(0, l)  # Не движемся дальше левой границы
+    l = max(-(camera.width - WIN_WIDTH), l)  # Не движемся дальше правой границы
+    t = max(-(camera.height - WIN_HEIGHT), t)  # Не движемся дальше нижней границы
+    t = min(0, t)  # Не движемся дальше верхней границы
+
+    return Rect(l, t, w, h)
+
+
+def loadLevel():
+    global playerX, playerY  # объявляем глобальные переменные, это координаты героя
+
+    levelFile = open("map_1.txt")
+    line = " "
+    commands = []
+    while line[0] != "/":  # пока не нашли символ завершения файла
+        line = levelFile.readline()  # считываем построчно
+        if line[0] == "[":  # если нашли символ начала уровня
+            while line[0] != "]":  # то, пока не нашли символ конца уровня
+                line = levelFile.readline()  # считываем построчно уровень
+                if line[0] != "]":  # и если нет символа конца уровня
+                    endLine = line.find("|")  # то ищем символ конца строки
+                    level.append(line[0: endLine])  # и добавляем в уровень строку от начала до символа "|"
+
+        if line[0] != "":  # если строка не пустая
+            commands = line.split()  # разбиваем ее на отдельные команды
+            if len(commands) > 1:  # если количество команд > 1, то ищем эти команды
+                if commands[0] == "player":  # если первая команда - player
+                    playerX = int(commands[1])  # то записываем координаты героя
+                    playerY = int(commands[2])
+
+
+def main():
+    loadLevel()
+    pygame.init()  # Инициация PyGame, обязательная строчка
+    screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
+    pygame.display.set_caption("Guild of Heroes")  # Пишем в шапку
+
+    left = right = False  # по умолчанию - стоим
+    up = False
+    running = False
+
+    hero = Player(playerX, playerY)  # создаем героя по (x,y) координатам
+    entities.add(hero)
+
+    timer = pygame.time.Clock()
+    x = y = 0  # координаты
+    for row in level:  # вся строка
+        for col in row:  # каждый символ
+            if col == "-":
+                pf = Platform(x, y)
+                entities.add(pf)
+                platforms.append(pf)
+            if col == "P":
+                pr = Next_level(x, y)
+                entities.add(pr)
+                platforms.append(pr)
+                animatedEntities.add(pr)
+
+            x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
+        y += PLATFORM_HEIGHT  # то же самое и с высотой
+        x = 0  # на каждой новой строчке начинаем с нуля
+
+    total_level_width = len(level[0]) * PLATFORM_WIDTH  # Высчитываем фактическую ширину уровня
+    total_level_height = len(level) * PLATFORM_HEIGHT  # высоту
+
+    camera = Camera(camera_configure, total_level_width, total_level_height)
+    start_time = pygame.time.get_ticks()  # Запоминаем начальное время
+    while not hero.winner:  # Основной цикл программы
+        timer.tick(60)
+        for e in pygame.event.get():  # Обрабатываем события
+            if e.type == QUIT:
+                raise SystemExit
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                print(mouse_pos)
+            if e.type == KEYDOWN and e.key == K_UP:
+                up = True
+            if e.type == KEYDOWN and e.key == K_LEFT:
+                left = True
+            if e.type == KEYDOWN and e.key == K_RIGHT:
+                right = True
+            if e.type == KEYDOWN and e.key == K_LSHIFT:
+                running = True
+
+            if e.type == KEYUP and e.key == K_UP:
+                up = False
+            if e.type == KEYUP and e.key == K_RIGHT:
+                right = False
+            if e.type == KEYUP and e.key == K_LEFT:
+                left = False
+            if e.type == KEYUP and e.key == K_LSHIFT:
+                running = False
+
+        screen.blit(BACKGROUND_IMAGE, (0, 0))
+
+        animatedEntities.update()  # показываеaм анимацию
+        camera.update(hero)  # центризируем камеру относительно персонажа
+        hero.update(left, right, up, platforms)  # передвижение
+        for e in entities:
+            screen.blit(e.image, camera.apply(e))
+            # Получаем текущее время
+        current_time = pygame.time.get_ticks()
+        # Вычисляем прошедшее время
+        elapsed_time = current_time - start_time
+        font = pygame.font.Font("DreiFraktur.ttf", 17)
+        text = font.render(f"Время: {elapsed_time // 1000} сек", True, (50, 128, 68))
+        screen.blit(text, (623, 562))
+        pygame.display.update()  # обновление и вывод всех изменений на экран
+
+        if hero.winner:
+            win(screen)
+            total_time = (pygame.time.get_ticks() - start_time) // 1000
+            font = pygame.font.Font("DreiFraktur.ttf", 34)
+            text = font.render(f"Итоговое время: {total_time} сек", True, (162, 219, 176))
+            screen.blit(text, (236, 448))
+        pygame.display.update()  # обновление и вывод всех изменений на экран
+
+
+level = []
+entities = pygame.sprite.Group()  # Все объекты
+animatedEntities = pygame.sprite.Group()  # все анимированные объекты, за исключением героя
+monsters = pygame.sprite.Group()  # Все передвигающиеся объекты
+platforms = []  # то, во что мы будем врезаться или опираться
+if __name__ == "__main__":
+    main()
